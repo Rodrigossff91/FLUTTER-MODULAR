018102cac9c71d66450c5b43ea5523eb0e88f2ed
Rodrigo Ferreira <rodrigo@treeinova.com>
2022-03-17 17:28:12 -0300

    Modular

--- flutter_modular_exemplo/lib/app/app_module.dart
+++ flutter_modular_exemplo/lib/app/app_module.dart
@@ -1,3 +1,15 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_modular_exemplo/app/splash/splash_page.dart';
 
-class AppModule extends Module {}
+class AppModule extends Module {
+  @override
+  List<Bind> get binds => [];
+
+  @override
+  List<ModularRoute> get routes => [
+        ChildRoute(
+          '/',
+          child: (context, args) => const SplashPage(),
+        )
+      ];
+}
